9ed316bfde72725b83ac98d6930126f43ab97854
Victor Tejada Yau <victor.tejada@arcadia.com>
2022-01-22 23:49:32 -0500

    adding font awesome to project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faCreditCard, faExclamationCircle } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pay-form';
+  faCreditCard = faCreditCard;
+  faExclamationCircle = faExclamationCircle;
 }
