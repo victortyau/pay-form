6c9fe87f8ea77ee6a8783ef0e9779fc20e4e5832
Victor Tejada Yau <victor.tejada@arcadia.com>
2022-01-23 23:42:34 -0500

    working on fields and component integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,21 @@ export class AppComponent {
   title = 'pay-form';
   faCreditCard = faCreditCard;
   faExclamationCircle = faExclamationCircle;
+  name :string = "Nombre";
+  credit_card_number: string = "1234 1234 1234 1234";
+  expiration_date: string = "MM/YY";
+  cvv: string = "..";
+  address: string = "Av.morelos #123";
+  confirm_payment: string = "Confirmar pago";
+  payment_info: string = "Informacion de Pago";
+  full_name: string = "Nombre Completo";
+  card_number_label: string = "Numero de Tarjeta de Credito";
+  expiration_date_label: string = "Fecha vencimiento";
+  cvv_label: string = "CVV";
+  address_label: string = "Direccion";
+  verify_data_label: string = "Verificas que esta informacion es correcta";
+
+  make_payment() {
+    console.log("make payment");
+  }
 }
